69716545d87b93adf1cadba7d9493cbf3e8ede8d
Natalia Barbosa <48846184+natisbar@users.noreply.github.com>
2023-04-22 15:26:10 -0500

    Componente dudas OK

--- src/app/feature/dudas/dudas.component.ts
+++ src/app/feature/dudas/dudas.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./dudas.component.css']
 })
 export class DudasComponent {
+  public isActive: string;
+
+  public clicAcordion(element:any){
+    var acc = document.getElementsByClassName("accordion");
+    console.log(element.parentElement);
+    element.classList.toggle("active");
+    element.classList.toggle("con-margen");
+    element.nextElementSibling.classList.toggle("girar");
+    var panel = element.parentElement.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+  }
 
 }
